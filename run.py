05226dd0bd6a82456078adf896211a412cafcd25
Rolf de Jong <dejongjong@users.noreply.github.com>
2019-09-28 14:25:12 +0200

    script in other file, py 3 and api token in req

---
+++ run.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+from nextaction import next_action
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/<api_token>')
+def index(api_token):
+    next_action(api_token)
+    return 'Magic performed!'
+
+app.run(host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000)))
