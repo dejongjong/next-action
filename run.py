cbc60257b3f2deaa34f78b88b716cf966967eeef
Rolf de Jong <rolf.dejong@leaseplan.com>
2020-08-07 11:24:04 +0200

    Test connection

--- run.py
+++ run.py
@@ -8,8 +8,7 @@ app = Flask(__name__)
 
 @app.route('/<api_token>')
 def index(api_token):
-	next_action(api_token)
+	#next_action(api_token)
 	return 'Magic performed!'
 
 app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
